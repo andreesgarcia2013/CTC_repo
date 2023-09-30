568ecfd960bb813d39d1ab2715b69cb1226cbc1c
andreesgarcia2013 <18030514@itcelaya.edu.mx>
2023-09-30 13:56:16 -0600

    (feat){backend]: Api implemented

--- server_laravel/routes/api.php
+++ server_laravel/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\LikeController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,7 +14,18 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
+
+Route::prefix('v1')->group(function () {
+    Route::prefix('task')->controller(TaskController::class)->group(function () {
+        Route::get('/tasks', 'index');
+        Route::post('/create', 'store');
+        Route::delete('/delete/{id}', 'destroy');
+    });
+    Route::prefix('like')->controller(LikeController::class)->group(function () {
+        Route::post('/', 'store');
+    });
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
